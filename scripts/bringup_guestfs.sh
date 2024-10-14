ca0feb677759cb92a74b628745ae578db0d7f1c5
Daniel Gomez <da.gomez@samsung.com>
2024-10-14 15:52:26 +0200

    guestfs: bringup: check domstate output
    virsh domstate checks only if a domain is defined. Check if the output
    is running to avoid trying to start a domain that is already running.
    
    Signed-off-by: Daniel Gomez <da.gomez@samsung.com>

--- scripts/bringup_guestfs.sh
+++ scripts/bringup_guestfs.sh
@@ -334,10 +334,12 @@ do
 	# If the guest is already defined, then just stop what we're doing
 	# and plead to the developer to clean things up.
 	#
-	virsh domstate $name 1>/dev/null 2>&1
+	output_domstate=$(virsh domstate $name 2>/dev/null)
 	if [ $? -eq 0 ]; then
-		echo "Domain $name is already defined."
-		virsh start $name
+		echo "Domain $name is already defined. (state: $output_domstate)"
+		if [ "$output_domstate" != "running" ]; then
+			virsh start $name
+		fi
 		exit 0
 	fi
 

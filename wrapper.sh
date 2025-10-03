7b684c2a593208e734706cb33cb13bc66645f27d
Daniel Gomez <da.gomez@samsung.com>
2025-10-03 21:54:28 +0200

    add ocp monitor support wrapper
    Signed-off-by: Daniel Gomez <da.gomez@samsung.com>

--- wrapper.sh
+++ wrapper.sh
@@ -42,6 +42,7 @@ defconfigs/configs/ci.config \
 defconfigs/configs/advance_pool.config \
 defconfigs/configs/qemu_bin.config \
 defconfigs/configs/sysbench-postgresql-4k.config \
+defconfigs/configs/monitor_ocp_smart.config \
 defconfigs/configs/pcip.config \
 defconfigs/configs/experiment-0000.config
 
